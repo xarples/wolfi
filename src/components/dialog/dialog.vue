afaf957adb747c196f95aadf855ec1cde0ed8b55
Guillermo Lopez <guillermolopez2529@gmail.com>
2019-02-16 01:05:43 -0400

    fix: dialog styles fixed

--- src/components/dialog/dialog.vue
+++ src/components/dialog/dialog.vue
@@ -74,6 +74,7 @@ export default {
 }
 .wolfi-dialog {
   box-shadow: 0 11px 15px -7px rgba(0,0,0,.2), 0 24px 38px 3px rgba(0,0,0,.14), 0 9px 46px 8px rgba(0,0,0,.12);
+  border-radius: 4px;
   background: #fff;
   box-sizing: border-box;
   min-height: 170px;
