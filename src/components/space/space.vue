144096d42a98abe4b1c7de23156bc6e8cc5f4c08
Guillermo Lopez <guillermolopez2529@gmail.com>
2019-03-02 23:57:26 -0400

    feat: add space component

---
+++ src/components/space/space.vue
@@ -0,0 +1,31 @@
+<template>
+  <div :style="style">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WSpace',
+  props: {
+    height: {
+      type: [String, Number],
+      default: 0
+    },
+    /** if it is number the value will be in px */
+    width: {
+      type: [String, Number],
+      default: 0
+    }
+  },
+  computed: {
+    style () {
+      return {
+        display: 'inline-block',
+        height: `${this.height}${this.height !== 'string' && 'px'}`,
+        width: `${this.width}${this.width !== 'string' && 'px'}`
+      }
+    }
+  }
+}
+</script>
