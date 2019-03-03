d3fe651ddf82fcca35293e204b6e04797945a059
Guillermo Lopez <guillermolopez2529@gmail.com>
2019-03-02 23:57:08 -0400

    feat: add container component

---
+++ src/components/container/container.stories.js
@@ -0,0 +1,35 @@
+import { storiesOf } from '@storybook/vue'
+import { withKnobs } from '@storybook/addon-knobs'
+import VueInfoAddon from 'storybook-addon-vue-info'
+
+const stories = storiesOf('Container', module)
+
+stories.addDecorator(withKnobs)
+stories.addDecorator(VueInfoAddon)
+
+stories
+  .add('default', () => ({
+    template: `
+      <w-container :max-width="1280" :padding-lateral="32">
+        <w-typography align="center" variant="headline5">
+          fugiat nulla pariatur.
+        </w-typography>
+        <w-space :height="16" />
+        <w-typography>
+          <w-strong>Lorem ipsum dolor sit amet</w-strong>, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+          exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+          in reprehenderit in voluptate velit esse cillum dolore eu
+          fugiat nulla pariatur.
+        </w-typography>
+        <w-space :height="16" />
+        <w-typography>
+          <w-strong>Lorem ipsum dolor sit amet</w-strong>, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+          exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+          in reprehenderit in voluptate velit esse cillum dolore eu
+          fugiat nulla pariatur.
+        </w-typography>
+      </w-container>
+    `
+  }))
---
+++ src/components/container/container.vue
@@ -0,0 +1,42 @@
+<template>
+  <section :style="container">
+    <div :style="containerChild">
+      <slot />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'WContainer',
+  props: {
+    backgroundColor: {
+      type: String,
+      default: 'white'
+    },
+    maxWidth: {
+      type: [String, Number],
+      default: 0
+    },
+    paddingLateral: {
+      type: [String, Number],
+      default: 0
+    }
+  },
+  computed: {
+    container () {
+      return {
+        backgroundColor: this.$wolfiTheme.colors[this.backgroundColor],
+        paddingLeft: `${this.paddingLateral}${this.paddingLateral !== 'string' && 'px'}`,
+        paddingRight: `${this.paddingLateral}${this.paddingLateral !== 'string' && 'px'}`
+      }
+    },
+    containerChild () {
+      return {
+        margin: '0 auto',
+        maxWidth: `${this.maxWidth}${this.maxWidth !== 'string' && 'px'}`
+      }
+    }
+  }
+}
+</script>
