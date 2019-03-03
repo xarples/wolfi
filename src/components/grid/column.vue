5434cda92f9d0d6fd46458c4312d42aba05ddff2
Guillermo Lopez <guillermolopez2529@gmail.com>
2019-03-03 19:05:00 -0400

    fix: fix vertical align

--- src/components/grid/column.vue
+++ src/components/grid/column.vue
@@ -6,14 +6,9 @@ export default {
       type: Number,
       default: 0
     },
-    horizontalAlign: {
-      type: String,
-      default: 'top',
-      validator: val => ['top', 'center', 'bottom', 'between', 'around'].includes(val)
-    },
     verticalAlign: {
       type: String,
-      default: 'top',
+      default: 'center',
       validator: val => ['top', 'center', 'bottom', 'between', 'around'].includes(val)
     },
     width: {
@@ -33,24 +28,12 @@ export default {
 
       return alignments[this.verticalAlign]
     },
-    _horizontalAlign () {
-      const alignments = {
-        top: 'flex-start',
-        center: 'center',
-        bottom: 'flex-end',
-        between: 'space-between',
-        around: 'space-around'
-      }
-
-      return alignments[this.horizontalAlign]
-    },
     style () {
       const columns = this.$parent.$props.columns
       const gutter = this.$parent.$props.gutter
 
       return {
         '--gutter': `${gutter / 2}px`,
-        '--horizontal-align': this._horizontalAlign,
         '--marginLeftA': `calc(100% / ${columns} * ${this.offset})`,
         '--marginLeftB': `calc(var(--marginLeftA) + ${gutter / 2}px)`,
         '--margin-left': this.offset ? 'var(--marginLeftB)' : `${gutter / 2}px`,
@@ -76,11 +59,9 @@ export default {
 
 <style scoped>
 .w-column {
-  align-items: var(--vertical-align);
+  align-self: var(--vertical-align);
   box-sizing: border-box;
-  display: flex;
   flex: 0 0 var(--widthB);
-  justify-content: var(--horizontal-align);
   margin: 0 var(--gutter);
   margin-left: var(--margin-left)
 }
