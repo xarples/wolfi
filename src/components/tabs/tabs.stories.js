8f692636408ad8e959eb799ff4d147a6c7d750b4
Guillermo Lopez <guillermolopez2529@gmail.com>
2019-04-15 19:05:29 -0400

    feat: add other tab example

--- src/components/tabs/tabs.stories.js
+++ src/components/tabs/tabs.stories.js
@@ -32,3 +32,27 @@ stories
   }), {
     info: {}
   })
+  .add('without full width', () => ({
+    data () {
+      return {
+        current: 1
+      }
+    },
+    methods: {
+      handleClick (e, tabIndex) {
+        this.current = tabIndex
+      }
+    },
+    components: { WTabs, WTab },
+    template: `
+      <div>
+        <w-tabs backgroundColor="white" color="primary">
+          <w-tab icon="clock" :active="current === 1" @click="handleClick($event, 1)">Recents</w-tab>
+          <w-tab icon="map-marker" :active="current === 2" @click="handleClick($event, 2)">Nearby</w-tab>
+          <w-tab icon="heart" :active="current === 3" @click="handleClick($event, 3)">Favorites</w-tab>
+        </w-tabs>
+      </div>
+    `
+  }), {
+    info: {}
+  })
