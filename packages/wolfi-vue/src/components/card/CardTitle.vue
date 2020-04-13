47517cb4716c64dcd30f202e23e2d06f075fa24f
Guillermo <guillermo@mantl.com>
2020-04-13 19:16:26 -0400

    fix: add align prop to card title

--- packages/wolfi-vue/src/components/card/CardTitle.vue
+++ packages/wolfi-vue/src/components/card/CardTitle.vue
@@ -1,11 +1,16 @@
 <script lang="ts">
 import { defineComponent } from "@vue/composition-api"
 import WText from "@/components/text/Text.vue"
+import { Align } from "@/components/text/types"
 import { Color } from "@/types"
 
 export default defineComponent({
   name: "WCardTitle",
   props: {
+    align: {
+      type: String as () => Align,
+      default: "default" as Align
+    },
     color: {
       type: String as () => Color,
       default: "default" as Color
@@ -18,7 +23,7 @@ export default defineComponent({
 </script>
 
 <template>
-  <w-text variant="h4" class="card-title" :color="color">
+  <w-text variant="h4" class="card-title" :color="color" :align="align">
     <slot />
   </w-text>
 </template>
