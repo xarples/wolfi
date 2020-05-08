f9eabef8589c6d7932be0459057dc8b208ca21fe
Jorge Cuevas <jcq012@gmail.com>
2020-05-07 23:54:28 -0400

    feat(bao): start wolfi react docs project

---
+++ packages/wolfi-react-docs/src/App.js
@@ -0,0 +1,12 @@
+import { Button } from "@xarples/wolfi-react"
+import React, { Component } from "react"
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <button>dd</button>
+      </div>
+    )
+  }
+}
--- packages/wolfi-react/rollup.config.js
+++ packages/wolfi-react/rollup.config.js
@@ -1,7 +1,12 @@
+import path from "path"
+
+import alias from "@rollup/plugin-alias"
 import commonjs from "@rollup/plugin-commonjs"
 import babel from "rollup-plugin-babel"
 
 import pkg from "./package.json"
+
+const projectRoot = path.resolve(__dirname, ".")
 
 export default {
   input: "src/index.tsx",
@@ -19,5 +24,14 @@ export default {
       sourcemap: true,
     },
   ],
-  plugins: [babel(), commonjs()],
+  plugins: [
+    // alias({
+    //   resolve: [".js", ".jsx", ".ts", ".tsx"],
+    //   entries: {
+    //     "@": path.resolve(projectRoot, "src"),
+    //   },
+    // }),
+    babel(),
+    commonjs(),
+  ],
 }
