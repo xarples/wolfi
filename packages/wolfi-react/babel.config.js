36a57c084d2c0f30d0d23d8434885239bf9d1bcf
Guillermo <guillermo@mantl.com>
2020-05-13 20:51:20 -0400

    fix: typescript preset

--- packages/wolfi-react/babel.config.js
+++ packages/wolfi-react/babel.config.js
@@ -2,7 +2,7 @@ module.exports = {
   presets: [
     "@babel/preset-env",
     "@babel/preset-react",
-    ["@babel/preset-typescript", { allExtensions: true }],
+    "@babel/preset-typescript"
   ],
-  plugins: ["styled-jsx/babel"],
+  plugins: ["styled-jsx/babel"]
 }
